a45f2fc11d2250a2f2b29379867e3330e1266329
Taylir <tdl9716@gmail.com>
2024-03-20 06:10:05 -0400

    Add and fix JS bugs and features to allow better user experience

--- JS/main.js
+++ JS/main.js
@@ -10,6 +10,34 @@ const workModals = [
         </a>
       </div>
     </div>`,
+    modalId: "web-1",
+    modal: `<div class="modal" id="web-1" data-animation="slideInOutTop">
+    <div class="modal-dialog">
+      <header class="modal-header">
+        <h3>Web Project 1</h3>
+        <i class="fas fa-times" data-close></i>
+      </header>
+      <div class="modal-body">
+        <div class="img-wrapper">
+          <img
+            src="./assets/images/portfolio-1.jpg"
+            alt="portfolio-1 icon"
+          />
+        </div>
+        <div class="text-wrapper">
+          <p><strong>My First Awesome Website</strong></p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi,
+            aperiam. Inventore hic consequuntur porro et?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Maxime, minima!
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>`,
   },
   {
     type: "web",
@@ -22,6 +50,34 @@ const workModals = [
         </a>
       </div>
     </div>`,
+    modalId: "web-2",
+    modal: `<div class="modal" id="web-2" data-animation="slideInOutTop">
+    <div class="modal-dialog">
+      <header class="modal-header">
+        <h3>Web Project 2</h3>
+        <i class="fas fa-times" data-close></i>
+      </header>
+      <div class="modal-body">
+        <div class="img-wrapper">
+          <img
+            src="./assets/images/portfolio-2.jpg"
+            alt="portfolio-1 icon"
+          />
+        </div>
+        <div class="text-wrapper">
+          <p><strong>My First Awesome Website</strong></p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi,
+            aperiam. Inventore hic consequuntur porro et?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Maxime, minima!
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>`,
   },
   {
     type: "web",
@@ -32,6 +88,34 @@ const workModals = [
         <div>Web Development</div>
         <h3>Food Website</h3>
       </a>
+    </div>
+  </div>`,
+    modalId: "web-3",
+    modal: `<div class="modal" id="web-3" data-animation="slideInOutTop">
+    <div class="modal-dialog">
+      <header class="modal-header">
+        <h3>Web Project 3</h3>
+        <i class="fas fa-times" data-close></i>
+      </header>
+      <div class="modal-body">
+        <div class="img-wrapper">
+          <img
+            src="./assets/images/portfolio-3.jpg"
+            alt="portfolio-1 icon"
+          />
+        </div>
+        <div class="text-wrapper">
+          <p><strong>My First Awesome Website</strong></p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi,
+            aperiam. Inventore hic consequuntur porro et?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Maxime, minima!
+          </p>
+        </div>
+      </div>
     </div>
   </div>`,
   },
@@ -46,6 +130,34 @@ const workModals = [
       </a>
     </div>
   </div>`,
+    modalId: "ui-1",
+    modal: `<div class="modal" id="ui-1" data-animation="slideInOutTop">
+  <div class="modal-dialog">
+    <header class="modal-header">
+      <h3>UI 1</h3>
+      <i class="fas fa-times" data-close></i>
+    </header>
+    <div class="modal-body">
+      <div class="img-wrapper">
+        <img
+          src="./assets/images/portfolio-4.jpg"
+          alt="portfolio-1 icon"
+        />
+      </div>
+      <div class="text-wrapper">
+        <p><strong>My First Awesome Website</strong></p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi,
+          aperiam. Inventore hic consequuntur porro et?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Maxime, minima!
+        </p>
+      </div>
+    </div>
+  </div>
+</div>`,
   },
   {
     type: "ui",
@@ -58,6 +170,34 @@ const workModals = [
       </a>
     </div>
   </div>`,
+    modalId: "ui-2",
+    modal: `<div class="modal" id="ui-2" data-animation="slideInOutTop">
+  <div class="modal-dialog">
+    <header class="modal-header">
+      <h3>UI 2</h3>
+      <i class="fas fa-times" data-close></i>
+    </header>
+    <div class="modal-body">
+      <div class="img-wrapper">
+        <img
+          src="./assets/images/portfolio-5.jpg"
+          alt="portfolio-1 icon"
+        />
+      </div>
+      <div class="text-wrapper">
+        <p><strong>My First Awesome Website</strong></p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi,
+          aperiam. Inventore hic consequuntur porro et?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Maxime, minima!
+        </p>
+      </div>
+    </div>
+  </div>
+</div>`,
   },
   {
     type: "app",
@@ -70,6 +210,34 @@ const workModals = [
       </a>
     </div>
   </div>`,
+    modalId: "app-1",
+    modal: `<div class="modal" id="app-1" data-animation="slideInOutTop">
+  <div class="modal-dialog">
+    <header class="modal-header">
+      <h3>App Project 1</h3>
+      <i class="fas fa-times" data-close></i>
+    </header>
+    <div class="modal-body">
+      <div class="img-wrapper">
+        <img
+          src="./assets/images/portfolio-6.jpg"
+          alt="portfolio-1 icon"
+        />
+      </div>
+      <div class="text-wrapper">
+        <p><strong>My First Awesome Website</strong></p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi,
+          aperiam. Inventore hic consequuntur porro et?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Maxime, minima!
+        </p>
+      </div>
+    </div>
+  </div>
+</div>`,
   },
   {
     type: "app",
@@ -82,6 +250,34 @@ const workModals = [
       </a>
     </div>
   </div>`,
+    modalId: "app-2",
+    modal: `<div class="modal" id="app-2" data-animation="slideInOutTop">
+  <div class="modal-dialog">
+    <header class="modal-header">
+      <h3>App Project 2</h3>
+      <i class="fas fa-times" data-close></i>
+    </header>
+    <div class="modal-body">
+      <div class="img-wrapper">
+        <img
+          src="./assets/images/portfolio-7.jpg"
+          alt="portfolio-1 icon"
+        />
+      </div>
+      <div class="text-wrapper">
+        <p><strong>My First Awesome Website</strong></p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi,
+          aperiam. Inventore hic consequuntur porro et?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Maxime, minima!
+        </p>
+      </div>
+    </div>
+  </div>
+</div>`,
   },
   {
     type: "app",
@@ -94,82 +290,141 @@ const workModals = [
       </a>
     </div>
   </div>`,
+    modalId: "app-3",
+    modal: `<div class="modal" id="app-3" data-animation="slideInOutTop">
+  <div class="modal-dialog">
+    <header class="modal-header">
+      <h3>App Project 3</h3>
+      <i class="fas fa-times" data-close></i>
+    </header>
+    <div class="modal-body">
+      <div class="img-wrapper">
+        <img
+          src="./assets/images/portfolio-8.jpg"
+          alt="portfolio-1 icon"
+        />
+      </div>
+      <div class="text-wrapper">
+        <p><strong>My First Awesome Website</strong></p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi,
+          aperiam. Inventore hic consequuntur porro et?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Maxime, minima!
+        </p>
+      </div>
+    </div>
+  </div>
+</div>`,
   },
 ];
 
-const theme = "theme";
+//Theme Vars
 const dataTheme = "data-theme";
-const themeTab = ".theme-tab";
-const switcherBtn = ".switcher-btn";
-const dark = "dark";
-const light = "light";
-const open = "theme-open";
-const active = "active";
-
-const modalOpen = "[data-open]";
-const modalClose = "[data-close]";
 const isVisable = "is-visable";
-
-const dataFilter = "[data-filter]";
-const portfolioData = "[data-item]";
-
 const root = document.documentElement;
+const chevronTab = document.querySelector(".theme-tab");
+const switcher = document.querySelectorAll(".switcher-btn");
+const currentTheme = localStorage.getItem("theme");
 
-//Theme
-const chevronTab = document.querySelector(themeTab);
-const switcher = document.querySelectorAll(switcherBtn);
-const currentTheme = localStorage.getItem(theme);
+//Modal vars
+const modalHolder = document.querySelector(".modal__holder");
+const portGrid = document.querySelector(".portfolio-grid");
+workModals.forEach((card) => (portGrid.innerHTML += card.html));
+const openModal = document.querySelectorAll("[data-open]");
+
+function closeOpenModal() {
+  const closeModal = document.querySelectorAll("[data-close]");
+  for (const elm of closeModal) {
+    elm.addEventListener("click", () => {
+      elm.closest(".modal, .full-site__modal").classList.remove(isVisable);
+    });
+  }
+}
+
+function openModalListener(elm) {
+  elm.addEventListener("click", function () {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisable);
+  });
+}
+
+function fullSiteModal() {
+  for (const elm of openModal) {
+    openModalListener(elm);
+    closeOpenModal();
+  }
+}
+
+function portModals() {
+  const activeModals = document.querySelectorAll(".portfolio-card");
+  for (const modal of activeModals) {
+    modal.addEventListener("click", (e) => {
+      const modalWant = e.target.parentElement.parentElement.dataset.open;
+      for (const modalPopup of workModals) {
+        if (modalPopup.modalId === modalWant) {
+          modalHolder.innerHTML = modalPopup.modal;
+          document.querySelector(".modal").classList.add(isVisable);
+          closeOpenModal();
+        }
+      }
+    });
+  }
+}
+
+// Port Vars
+const filterLink = document.querySelectorAll("[data-filter]");
+const portfolioItems = document.querySelectorAll("[data-item]");
+const searchBox = document.querySelector("#search");
 
 if (currentTheme) {
   root.setAttribute(dataTheme, currentTheme);
   switcher.forEach((btn) => {
-    btn.classList.remove(active);
+    btn.classList.remove("active");
   });
 
-  if (currentTheme === dark) {
-    switcher[1].classList.add(active);
+  if (currentTheme === "dark") {
+    switcher[1].classList.add("active");
   } else {
-    switcher[0].classList.add(active);
+    switcher[0].classList.add("active");
   }
 }
 
 const setActive = (elm, selector) => {
-  if (document.querySelector(`${selector}.${active}`)) {
-    document.querySelector(`${selector}.${active}`).classList.remove(active);
+  if (document.querySelector(`${selector}.${"active"}`)) {
+    document
+      .querySelector(`${selector}.${"active"}`)
+      .classList.remove("active");
   }
-  elm.classList.add(active);
+  elm.classList.add("active");
 };
 
 const setTheme = (val) => {
-  if (val === dark) {
-    root.setAttribute(dataTheme, dark);
-    localStorage.setItem(theme, dark);
+  if (val === "dark") {
+    root.setAttribute(dataTheme, "dark");
+    localStorage.setItem("theme", "dark");
   } else {
-    root.setAttribute(dataTheme, light);
-    localStorage.setItem(theme, light);
+    root.setAttribute(dataTheme, "light");
+    localStorage.setItem("theme", "light");
   }
 };
 
 chevronTab.addEventListener("click", function () {
   const tab = this.parentElement.parentElement;
-  tab.classList.toggle(open);
+  tab.classList.toggle("theme-open");
 });
 
 for (const elm of switcher) {
   elm.addEventListener("click", function () {
     const toggle = this.dataset.toggle;
-    setActive(elm, switcherBtn);
+    setActive(elm, ".switcher-btn");
     setTheme(toggle);
   });
 }
 
 // Portfolio
-const portGrid = document.querySelector(".portfolio-grid");
-workModals.forEach((card) => (portGrid.innerHTML += card.html));
-
-const filterLink = document.querySelectorAll(dataFilter);
-const portfolioItems = document.querySelectorAll(portfolioData);
-const searchBox = document.querySelector("#search");
 
 for (const link of filterLink) {
   link.addEventListener("click", function () {
@@ -179,9 +434,11 @@ for (const link of filterLink) {
     workModals.forEach((card) => {
       if (card.type == filter) {
         portGrid.innerHTML += card.html;
+        portModals();
       }
       if (filter == "all") {
         portGrid.innerHTML += card.html;
+        portModals();
       }
     });
   });
@@ -193,43 +450,28 @@ searchBox.addEventListener("keyup", (e) => {
   workModals.forEach((card) => {
     if (card.type.includes(searchInput)) {
       portGrid.innerHTML += card.html;
+      portModals();
     }
     if (e == "") {
       portGrid.innerHTML += card.html;
+      portModals();
     }
   });
 });
 
 //Modals
-const openModal = document.querySelectorAll(modalOpen);
-const closeModal = document.querySelectorAll(modalClose);
 
-for (const elm of openModal) {
-  elm.addEventListener("click", function () {
-    const modalId = this.dataset.open;
-    document.getElementById(modalId).classList.add(isVisable);
-    closeOpenModal();
-  });
-}
+portModals();
+fullSiteModal();
 
-function closeOpenModal() {
-  for (const elm of closeModal) {
-    elm.addEventListener("click", function () {
-      this.parentElement.parentElement.parentElement.classList.remove(
-        isVisable
-      );
-    });
-  }
-}
-
-//modal
-
+//Closes modal on background click
 document.addEventListener("click", (e) => {
   if (e.target === document.querySelector(".modal.is-visable")) {
     document.querySelector(".modal.is-visable").classList.remove(isVisable);
   }
 });
 
+//closes modal with esc
 document.addEventListener("keyup", (e) => {
   if (e.key === "Escape") {
     document.querySelector(".modal.is-visable").classList.remove(isVisable);
